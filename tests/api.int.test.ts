eff614328502596c99ef39c15a032a0b01cd1c5c
Rajiv Mounguengue <rajiv.mounguengue@functionalstack.io>
2022-03-19 22:12:31 +0100

    chore: wip

---
+++ tests/api.int.test.ts
@@ -0,0 +1,12 @@
+
+describe("Predict values", () => {
+  
+    it("Zero", async () => {
+      expect(-1).toBe(-1);
+    });
+  
+    it("Two", async () => {
+      expect(3).toBe(3);
+    });
+  
+  });
